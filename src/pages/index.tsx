120ab0cb00a5f9eec59853b0042488e922cbb2bd
AlisonMarques <alison-marques@outlook.com.br>
2021-08-10 00:53:17 -0300

    Estrutura inicial do projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/template/Layout';
+
 export default function Home() {
   return (
-    <div
-      className={`
-      flex justify-center items-center h-screen
-      bg-gradient-to-r from-green-500 to-blue-500
-    `}
+    <Layout
+      titulo="Página Inicial"
+      subtitulo="Estamos construindo um template admin"
     >
-      <h1>Admin</h1>
-    </div>
+      <h3>Conteúdo</h3>
+    </Layout>
   );
 }

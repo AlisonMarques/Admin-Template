deedb87227416d4a3c6f47836f1d720670cbb253
AlisonMarques <alison-marques@outlook.com.br>
2021-08-10 00:27:49 -0300

    Configuração inicial do projeto com TailWind e TS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+//Purge vai detectar quais são os lugares que suas classes de css estão usando Tailwind
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
